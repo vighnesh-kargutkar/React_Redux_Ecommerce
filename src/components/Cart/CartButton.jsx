9e59b6205486f5b9e63e50b126b0a12eb99c26ba
vighnesh Kargutkar <kargutkar860@gmail.com>
2024-12-29 18:27:09 +0530

    final push

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,8 +1,14 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const uiDispatch = useDispatch();
+  function handleToggle() {
+    uiDispatch(uiActions.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleToggle}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
